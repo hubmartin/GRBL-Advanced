993dabe2ed9d8ddc14e7ac625b5df3e9eb9986ff
hubmartin <hub.martin@gmail.com>
2019-01-09 00:01:59 +0100

    First alpha with bugs

--- HAL_F3/STM32F3/stm32f30x_it.c
+++ HAL_F3/STM32F3/stm32f30x_it.c
@@ -29,6 +29,14 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f30x_it.h"
+#include "USART.h"
+#include "FIFO_USART.h"
+#include "Limits.h"
+#include "Stepper.h"
+#include "System.h"
+#include "Settings.h"
+#include "Config.h"
+#include "MotionControl.h"
 
 /** @addtogroup STM32F30x_StdPeriph_Templates
   * @{
@@ -38,8 +46,18 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+volatile uint8_t DebounceCounterControl = 0;
+volatile uint8_t DebounceCounterLimits = 0;
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
+
+extern void Limit_PinChangeISR(void);
+extern void System_PinChangeISR(void);
+
+
+// Counter for milliseconds
+static volatile uint32_t gMillis = 0;
+
 
 /******************************************************************************/
 /*            Cortex-M4 Processor Exceptions Handlers                         */
@@ -140,6 +158,37 @@ void PendSV_Handler(void)
   */
 void SysTick_Handler(void)
 {
+  	/*
+	 * Because of the board layout, we cant attach all pins to interrupts.
+	 * Therefore we just poll them in this 1ms task, which is hopefully fast
+	 * enough for critical events. Debouncing pins is also implemented here.
+	 */
+	uint8_t limits = Limits_GetState();
+	if(limits) {
+		// X-Y-Z Limit
+		if((DebounceCounterLimits == 0) && settings.system_flags & BITFLAG_ENABLE_LIMITS) {
+			DebounceCounterLimits = 20;
+			Limit_PinChangeISR();
+		}
+	}
+
+	uint8_t controls = System_GetControlState();
+	if(controls) {
+		// System control
+		if(DebounceCounterControl == 0) {
+			DebounceCounterControl = 20;
+			System_PinChangeISR();
+		}
+	}
+
+	if(DebounceCounterLimits && !limits) {
+		DebounceCounterLimits--;
+	}
+	if(DebounceCounterControl && !controls) {
+		DebounceCounterControl--;
+	}
+
+	gMillis++;
 }
 
 /******************************************************************************/
@@ -148,6 +197,108 @@ void SysTick_Handler(void)
 /*  available peripheral interrupt handler's name please refer to the startup */
 /*  file (startup_stm32f30x.s).                                            */
 /******************************************************************************/
+
+/**
+  * @brief  This function handles TIM4 global interrupt request.
+  * @param  None
+  * @retval None
+  */
+void TIM4_IRQHandler(void)
+{
+	/* TIM4_CH1 */
+	if(TIM_GetITStatus(TIM4, TIM_IT_CC1) != RESET) {
+		// OC
+		Stepper_MainISR();
+
+		TIM_ClearITPendingBit(TIM4, TIM_IT_CC1);
+	}
+	else if(TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET) {
+		// OVF
+		Stepper_PortResetISR();
+
+		TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+	}
+}
+
+
+/**
+  * @brief  This function handles USART2 global interrupt request.
+  * @param  None
+  * @retval None
+  */
+void USART2_IRQHandler(void)
+{
+	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET) {
+		/* Read one byte from the receive data register */
+		unsigned char c = (USART_ReceiveData(USART2) & 0xFF);
+
+		// Pick off realtime command characters directly from the serial stream. These characters are
+		// not passed into the main buffer, but these set system state flag bits for realtime execution.
+		switch(c)
+		{
+		case CMD_RESET:         MC_Reset(); break; // Call motion control reset routine.
+		case CMD_STATUS_REPORT: System_SetExecStateFlag(EXEC_STATUS_REPORT);break;
+		case CMD_CYCLE_START:   System_SetExecStateFlag(EXEC_CYCLE_START); break; // Set as true
+		case CMD_FEED_HOLD:     System_SetExecStateFlag(EXEC_FEED_HOLD); break; // Set as true
+		default:
+			if(c > 0x7F) { // Real-time control characters are extended ACSII only.
+				switch(c)
+				{
+				case CMD_SAFETY_DOOR: System_SetExecStateFlag(EXEC_SAFETY_DOOR); break; // Set as true
+				case CMD_JOG_CANCEL:
+					if(sys.state & STATE_JOG) { // Block all other states from invoking motion cancel.
+						System_SetExecStateFlag(EXEC_MOTION_CANCEL);
+					}
+					break;
+
+				case CMD_FEED_OVR_RESET: System_SetExecMotionOverrideFlag(EXEC_FEED_OVR_RESET); break;
+				case CMD_FEED_OVR_COARSE_PLUS: System_SetExecMotionOverrideFlag(EXEC_FEED_OVR_COARSE_PLUS); break;
+				case CMD_FEED_OVR_COARSE_MINUS: System_SetExecMotionOverrideFlag(EXEC_FEED_OVR_COARSE_MINUS); break;
+				case CMD_FEED_OVR_FINE_PLUS: System_SetExecMotionOverrideFlag(EXEC_FEED_OVR_FINE_PLUS); break;
+				case CMD_FEED_OVR_FINE_MINUS: System_SetExecMotionOverrideFlag(EXEC_FEED_OVR_FINE_MINUS); break;
+				case CMD_RAPID_OVR_RESET: System_SetExecMotionOverrideFlag(EXEC_RAPID_OVR_RESET); break;
+				case CMD_RAPID_OVR_MEDIUM: System_SetExecMotionOverrideFlag(EXEC_RAPID_OVR_MEDIUM); break;
+				case CMD_RAPID_OVR_LOW: System_SetExecMotionOverrideFlag(EXEC_RAPID_OVR_LOW); break;
+				case CMD_SPINDLE_OVR_RESET: System_SetExecAccessoryOverrideFlag(EXEC_SPINDLE_OVR_RESET); break;
+				case CMD_SPINDLE_OVR_COARSE_PLUS: System_SetExecAccessoryOverrideFlag(EXEC_SPINDLE_OVR_COARSE_PLUS); break;
+				case CMD_SPINDLE_OVR_COARSE_MINUS: System_SetExecAccessoryOverrideFlag(EXEC_SPINDLE_OVR_COARSE_MINUS); break;
+				case CMD_SPINDLE_OVR_FINE_PLUS: System_SetExecAccessoryOverrideFlag(EXEC_SPINDLE_OVR_FINE_PLUS); break;
+				case CMD_SPINDLE_OVR_FINE_MINUS: System_SetExecAccessoryOverrideFlag(EXEC_SPINDLE_OVR_FINE_MINUS); break;
+				case CMD_SPINDLE_OVR_STOP: System_SetExecAccessoryOverrideFlag(EXEC_SPINDLE_OVR_STOP); break;
+				case CMD_COOLANT_FLOOD_OVR_TOGGLE: System_SetExecAccessoryOverrideFlag(EXEC_COOLANT_FLOOD_OVR_TOGGLE); break;
+#ifdef ENABLE_M7
+				case CMD_COOLANT_MIST_OVR_TOGGLE: System_SetExecAccessoryOverrideFlag(EXEC_COOLANT_MIST_OVR_TOGGLE); break;
+#endif
+				}
+			// Throw away any unfound extended-ASCII character by not passing it to the serial buffer.
+			}
+			else {
+				// Write character to buffer
+				FifoUsart_Insert(USART2_NUM, USART_DIR_RX, c);
+			}
+		}
+	}
+
+	if(USART_GetITStatus(USART2, USART_IT_TXE) != RESET) {
+		char c;
+
+		if(FifoUsart_Get(USART2_NUM, USART_DIR_TX, &c) == 0) {
+			/* Write one byte to the transmit data register */
+			while(USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET);
+			USART_SendData(USART2, c);
+		}
+		else {
+			// Nothing to transmit - disable interrupt
+			USART_ITConfig(USART2, USART_IT_TXE, DISABLE);
+		}
+	}
+
+	/* If overrun condition occurs, clear the ORE flag and recover communication */
+	if(USART_GetFlagStatus(USART2, USART_FLAG_ORE) != RESET) {
+		//(void)USART_ReceiveData(USART2);
+    USART_ClearFlag(USART2, USART_FLAG_ORE);
+	}
+}
 
 /**
   * @brief  This function handles PPP interrupt request.
